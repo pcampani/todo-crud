1cf5a9943a9d4df6786175b07b6b32285a94b2eb
pcampani <toycampani@gmail.com>
2020-05-07 23:24:47 +0800

    Additional package installation

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './slices/taskSlice';
+import Dashboard from './Dashboard';
+
+const store = configureStore({reducer});
 
 
 export default function App() {
 	return (
-			<h1>Hello World!!!</h1>
+			<Provider store={store}>
+				<Dashboard />
+			</Provider>
 	);
 }
--- src/components/slices/taskSlice.js
+++ src/components/slices/taskSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { db } from '../firebase/config';
 
 const initialState = {
 	loading: false,
@@ -6,5 +7,40 @@ const initialState = {
 	tasks: []
 };
 
+const taskSlice = createSlice({
+	name: 'task',
+	initialState,
+	reducers: {
+		getTasks(state) {
+			state.loading = true;
+		},
+		getSuccess(state, {payload}) {
+			state.loading = false;
+			state.tasks = payload;
+		},
+		displayError(state, {payload}) {
+			state.hasErrors = true;
+			console.log('Error occurred: ', payload);
+		}
+	}
+});
 
+export const taskSelector = state => state;
+export const { getTasks, getSuccess, displayError } = taskSlice.actions;
+export default taskSlice.reducer;
 
+export function fetch() {
+	return async dispatch => {
+		dispatch(getTasks());
+
+		try {
+			let tasksLists = [];
+			const snapshot = await db.collection('todos').get();
+			snapshot.docs.forEach(doc => tasksLists.push({...doc.data(), key:doc.id}));
+			dispatch(getSuccess(tasksLists));
+		}
+		catch(error) {
+			dispatch(displayError(error));
+		}
+	};
+}
